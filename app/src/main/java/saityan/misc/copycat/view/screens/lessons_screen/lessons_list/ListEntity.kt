ff564e589592bd310a2f706a58a6f5fda29ad8fa
saityan <naminatosky@gmail.com>
2022-11-19 21:24:16 +0300

    List Entity acknowledged

---
+++ app/src/main/java/saityan/misc/copycat/view/screens/lessons_screen/lessons_list/ListEntity.kt
@@ -0,0 +1,15 @@
+package saityan.misc.copycat.view.screens.lessons_screen.lessons_list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ListEntity() {
+
+}
+
+@Composable
+@Preview(showBackground = false)
+fun ListEntityPreview() {
+    ListEntity()
+}
