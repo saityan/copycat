b0b29e9375677a8e1c4dd88c3648a14b04029acf
saityan <naminatosky@gmail.com>
2022-11-21 21:12:52 +0300

    minor adjustments to the list

---
+++ app/src/main/java/saityan/misc/copycat/view/screens/lessons_screen/lessons_list/LessonsList.kt
@@ -0,0 +1,28 @@
+package saityan.misc.copycat.view.screens.lessons_screen.lessons_list
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LessonsList() {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(all = 6.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+
+    }
+}
+
+@Composable
+@Preview(showBackground = false)
+fun LessonListPreview() {
+    LessonsList()
+}
