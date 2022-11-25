25899387cb49da2d0f36fd2b9bdccf251a630492
saityan <naminatosky@gmail.com>
2022-11-25 18:45:09 +0300

    Lesson simplified

--- app/src/main/java/saityan/misc/copycat/model/Lesson.kt
+++ app/src/main/java/saityan/misc/copycat/model/Lesson.kt
@@ -1,15 +1,7 @@
 package saityan.misc.copycat.model
 
-import androidx.room.Embedded
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import kotlinx.serialization.Serializable
-
-@Serializable
-@Entity(tableName = "classes_table")
 data class Lesson(
-    @PrimaryKey(autoGenerate = false)
     val id: String,
-    @Embedded
     val name: String,
+    val teacherName: String,
 )
