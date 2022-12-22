f6303a0ec5f3adc0b65f241538aa32d992583128
saityan <naminatosky@gmail.com>
2022-12-22 16:09:37 +0300

    icons colored

--- app/src/main/java/saityan/misc/copycat/view/screens/main_screen/homework/HomeworkBody.kt
+++ app/src/main/java/saityan/misc/copycat/view/screens/main_screen/homework/HomeworkBody.kt
@@ -1,13 +1,18 @@
 package saityan.misc.copycat.view.screens.main_screen.homework
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun HomeworkBody(desc: String) {
-    Text(
-        text = desc,
-        style = MaterialTheme.typography.subtitle2
-    )
+fun HomeworkBody(desc: String, icon: Int) {
+    Column() {
+        Text(
+            text = desc,
+            style = MaterialTheme.typography.subtitle2
+        )
+
+
+    }
 }
