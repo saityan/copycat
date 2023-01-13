47fe8ff2e176cd8e4dae6d075acb9dfe8d9859f6
saityan <naminatosky@gmail.com>
2023-01-13 20:12:05 +0300

    Added timer icon to lesson hours.

---
+++ app/src/main/java/saityan/misc/copycat/view/screens/common/TimerIcon.kt
@@ -0,0 +1,37 @@
+package saityan.misc.copycat.view.screens.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import saityan.misc.copycat.R
+import saityan.misc.copycat.ui.theme.Typography
+
+@Composable
+fun TimerIcon(paddingTop: Int) {
+    Box(
+        modifier = Modifier
+            .padding(top = paddingTop.dp)
+            .size(12.dp)
+            .background(color = Color.Transparent)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_clock),
+            contentDescription = "",
+            tint = Typography.subtitle2.color
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun TimerIconPreview() {
+    TimerIcon(12)
+}
--- app/src/main/java/saityan/misc/copycat/view/screens/main_screen/lessons/LessonTimeMain.kt
+++ app/src/main/java/saityan/misc/copycat/view/screens/main_screen/lessons/LessonTimeMain.kt
@@ -1,13 +1,32 @@
 package saityan.misc.copycat.view.screens.main_screen.lessons
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import saityan.misc.copycat.ui.theme.Typography
+import saityan.misc.copycat.view.screens.common.TimerIcon
 
 @Composable
 fun LessonTimeMain(lessonTime: String) {
-    Text(
-        text = lessonTime,
-        style = Typography.subtitle2
-    )
+    Row {
+        TimerIcon(12)
+
+        Text(
+            modifier = Modifier
+                .padding(vertical = 9.dp)
+                .padding(horizontal = 6.dp),
+            text = lessonTime,
+            style = Typography.subtitle2,
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = false)
+private fun LessonTimeMain() {
+    LessonTimeMain(lessonTime = "10:00 - 10:45")
 }
