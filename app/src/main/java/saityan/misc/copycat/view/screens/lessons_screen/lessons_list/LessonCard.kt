4a298f6a026b60f68ea72154efdb38127317029d
saityan <naminatosky@gmail.com>
2023-01-20 21:08:08 +0300

    added circle to lessons line (work in progress)

--- app/src/main/java/saityan/misc/copycat/view/screens/lessons_screen/lessons_list/LessonCard.kt
+++ app/src/main/java/saityan/misc/copycat/view/screens/lessons_screen/lessons_list/LessonCard.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,8 +33,9 @@ fun LessonCard(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                modifier = Modifier.offset(y = 18.dp),
-                imageVector = ImageVector.vectorResource(id = R.drawable.lessons_line),
+                modifier = Modifier
+                    .offset(y = 10.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.lessons_line_passive),
                 contentDescription = "",
             )
         }
