a7874ea701ad2cb9c4303ab5854de5a613e97c4f
saityan <naminatosky@gmail.com>
2023-01-24 16:30:01 +0300

    switched to Divider in LessonsList

--- app/src/main/java/saityan/misc/copycat/view/screens/lessons_screen/lessons_list/LessonCard.kt
+++ app/src/main/java/saityan/misc/copycat/view/screens/lessons_screen/lessons_list/LessonCard.kt
@@ -1,6 +1,6 @@
 package saityan.misc.copycat.view.screens.lessons_screen.lessons_list
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
@@ -8,13 +8,11 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import saityan.misc.copycat.R
 import saityan.misc.copycat.view.screens.common.ButtonOpenIn
 
 @Composable
@@ -22,22 +20,19 @@ fun LessonCard(
     subject: String,
     teacher: String,
     timePeriod: String,
-    isOpenedIn: Boolean = true
+    isOpenedIn: Boolean = true,
+    isPaired: Boolean = false
 ) {
     Row(
         modifier = Modifier
             .padding(end = 6.dp)
     ) {
         Column(
-            modifier = Modifier.weight(1.5f),
+            modifier = Modifier
+                .weight(1.5f),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                modifier = Modifier
-                    .offset(y = 9.dp),
-                painter = painterResource(id = R.drawable.lessons_line_short),
-                contentDescription = null,
-            )
+            VerticalDivider()
         }
 
         Column(
@@ -84,3 +79,19 @@ fun LessonCard(
 private fun LessonCardPreview() {
     LessonCard(subject = "History", teacher = "Mrs Thomas", timePeriod = "8:00 - 8:45")
 }
+
+@Composable
+fun VerticalDivider(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.secondary.copy(alpha = DividerAlpha),
+    thickness: Dp = 2.dp
+) {
+    Box(
+        modifier
+            .heightIn(175.dp, 350.dp)
+            .width(thickness)
+            .background(color = color)
+    )
+}
+
+private const val DividerAlpha = 1f
