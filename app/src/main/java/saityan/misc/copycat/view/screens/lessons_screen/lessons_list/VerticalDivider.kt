1b627631a3238c084fb00743743e6f825719fee2
saityan <naminatosky@gmail.com>
2023-01-28 14:56:55 +0300

    added dots to divider in LessonsList

--- app/src/main/java/saityan/misc/copycat/view/screens/lessons_screen/lessons_list/VerticalDivider.kt
+++ app/src/main/java/saityan/misc/copycat/view/screens/lessons_screen/lessons_list/VerticalDivider.kt
@@ -1,24 +1,34 @@
 package saityan.misc.copycat.view.screens.lessons_screen.lessons_list
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import saityan.misc.copycat.R
 
 @Composable
 fun VerticalDivider(
     modifier: Modifier = Modifier,
-    color: Color = MaterialTheme.colors.secondary.copy(alpha = 0.8f),
-    thickness: Dp = 2.dp,
+    color: Color = MaterialTheme.colors.secondary.copy(alpha = 0.3f),
+    thickness: Dp = 1.dp,
     isPaired: Boolean = false
 ) {
+    Image(
+        modifier = Modifier.offset(y = 10.dp),
+        imageVector = ImageVector.vectorResource(id = R.drawable.utility_dot),
+        contentDescription = null
+    )
+
     Box(
         modifier
-            .offset(y = 18.dp)
+            .offset(y = 10.dp)
             .heightIn(
                 if (isPaired)
                     275.dp
