29a4941d236259499ccb6917b92a9ca534cb89c9
saityan <naminatosky@gmail.com>
2023-03-04 13:33:14 +0300

    added splash screen

--- app/src/main/java/saityan/misc/copycat/MainActivity.kt
+++ app/src/main/java/saityan/misc/copycat/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import saityan.misc.copycat.ui.theme.CopycatTheme
@@ -13,6 +14,9 @@ import saityan.misc.copycat.view.bottom_bar.BottomBarScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
 
             HideSystemBars()
