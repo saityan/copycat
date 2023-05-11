e192c2784245ed46d0025472bcbc1c9bff33e600
saityan <naminatosky@gmail.com>
2023-05-11 08:42:35 +0300

    Commented out countdown timer section to test the app on my phone.

--- app/src/main/java/saityan/misc/copycat/view/screens/main_screen/MainScreen.kt
+++ app/src/main/java/saityan/misc/copycat/view/screens/main_screen/MainScreen.kt
@@ -1,6 +1,5 @@
 package saityan.misc.copycat.view.screens.main_screen
 
-import android.os.CountDownTimer
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -10,7 +9,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import saityan.misc.copycat.view.screens.common.countdown
 import saityan.misc.copycat.view.screens.main_screen.greeting.Greeting
 import saityan.misc.copycat.view.screens.main_screen.homework.Homework
 import saityan.misc.copycat.view.screens.main_screen.lessons.LessonMain
@@ -22,19 +20,23 @@ import java.time.format.DateTimeFormatter
 
 @Composable
 fun MainScreen() {
-    var formattedtime: String by remember {
+    /*
+
+    var formattedTime: String by remember {
         mutableStateOf("00:00:00")
     }
     val count = object : CountDownTimer(
         countdown, 60000
     ) {
         override fun onTick(millisUntilFinished: Long) {
-            formattedtime = formatTime(millisUntilFinished)
+            formattedTime = formatTime(millisUntilFinished)
         }
 
         override fun onFinish() {}
     }
     count.start()
+
+    */
 
     Surface(
         modifier = Modifier
@@ -50,7 +52,7 @@ fun MainScreen() {
             Column(
                 modifier = Modifier.verticalScroll(rememberScrollState())
             ) {
-                TimerCard(formattedtime)
+                TimerCard("10:00:00")
                 Spacer(modifier = Modifier.height(24.dp))
 
                 LessonMain(lessonsCount = 6, subject = "History", timePeriod = "8:00 — 8:45")
@@ -67,7 +69,7 @@ fun MainScreen() {
 fun MainScreenPreview() {
     MainScreen()
 }
-
+/*
 private fun formatTime(timeMillis: Long): String {
     val localDateTime = LocalDateTime.ofInstant(
         Instant.ofEpochMilli(timeMillis),
@@ -76,3 +78,4 @@ private fun formatTime(timeMillis: Long): String {
     val formatter = DateTimeFormatter.ofPattern("dd:hh:mm")
     return localDateTime.format(formatter)
 }
+*/
